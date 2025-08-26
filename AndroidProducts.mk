1d51d45bf08a1a39a3657cee59749f7d8d85a3a1
TARKZiM <tom8476oo@gmail.com>
2025-08-26 20:44:49 +0800

    gts8wifi: Initial device trees

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gts8wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_gts8wifi-user \
+    twrp_gts8wifi-userdebug \
+    twrp_gts8wifi-eng
---
+++ twrp_gts8wifi.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from gts8wifi device
+$(call inherit-product, device/samsung/gts8wifi/device.mk)
+
+PRODUCT_DEVICE := gts8wifi
+PRODUCT_NAME := twrp_gts8wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-X700
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gts8wifixx-user 12 SP2A.220305.013 X700XXS9DYF4 release-keys"
+
+BUILD_FINGERPRINT := samsung/gts8wifixx/gts8wifi:12/SP2A.220305.013/X700XXS9DYF4:user/release-keys
